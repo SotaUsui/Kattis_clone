c66bd15fa090e1920db981c637ee2585b6e92185
SotaUsui <126287918+SotaUsui@users.noreply.github.com>
2023-10-12 15:21:07 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <stdlib.h> 
 using namespace std;
 
 int main(int argc, char** argv){
+    cout << "Hello_1¥¥" << endl;
 
     if (argc != 3){
         cout << "syntax error"<< endl;
@@ -11,16 +13,22 @@ int main(int argc, char** argv){
     else{
         string program = argv[1];
         string submission_code = argv[2];
-        submission_code = "/user_code/" +submission_code;
-        // try
-        system("g++ " + submission_code + "-o testing")
 
+        cout << "program [" << program << "]" << endl;
 
+        cout << "submissison code [" << submission_code << "]" << endl;
+        submission_code = "user_code/" +submission_code;
+        cout << "submissison code [" << submission_code << "]" << endl;
 
+        string test = program + "/test1.input";
+        string cmd = "g++ -o testing " + submission_code;
+        cout << "executing cmd: [" << cmd << "]" << endl;
+        system(cmd.c_str());
 
-
-
-        system("testing > program/test1.input");
+        cmd = "./testing < " +test;
+        cout << "executing cmd: [" << cmd << "]" << endl;
+        system(cmd.c_str());
+        cout << "done" << endl;
     }
     return 0;
 }
